0e3248396cbbf2d035878e5c2f754059b4c8e242
zwa47 <zwa47@asb9838s-c01.csil.sfu.ca>
2014-02-18 12:21:20 -0800

    eisner debug

---
+++ eisner_test.py
@@ -0,0 +1,39 @@
+
+import eisner
+
+test_edge_score =
+{
+    ('ROOT','economic'): 1,
+    ('ROOT','news'): 1,
+    ('ROOT','had'): 10,
+    ('ROOT','little'): 1,
+    ('ROOT','effect'): 2,
+    ('ROOT','on'): 1,
+    ('ROOT','financial'): 3,
+    ('ROOT','markets'): 2,
+    ('ROOT','news'): 1,
+
+    ('had','news'): 12,
+    ('news','ecomonic'): 9,
+    ('had','effect'): 11,
+    ('effect','little'): 10,
+    ('effect','on'): 8,
+    ('on','markets'): 8,
+    ('markets','financial'): 7,
+
+    ('little','on'): 1,
+    ('markets','news'): 4,
+    ('had','financial'): 3,
+    ('effect','markets'): 1,
+    ('economic','had'): 2
+}
+
+def test_get_score(parent,child):
+    if test_edge_score.has_key((parent,child)):
+        return test_edge_score[(parent,child)]
+    else:
+        return 1
+
+def test_eisner():
+    sentence = "ROOT econimic news had little effect on financial markets".split()
+    print eisner(sentence,test_get_score)
