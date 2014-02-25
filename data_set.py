25e9807c5058f160fc3ba96e1658b6adea92a0cc
yulanh <yulanh@sfu.ca>
2014-02-25 11:52:54 -0800

    comment added

--- data_set.py
+++ data_set.py
@@ -6,27 +6,43 @@ class DataSet():
     """
     Read the test data files and generate the trees for parser testing
     The class is specifically for penn-wsj-deps data
-    
-    the section set can be defined as either tuple or list
-    but cannot be both at the same time
     """
     def __init__(self,  
                  section_set=[(2,21)], 
                  data_path=".\\penn-wsj-deps\\"):
-        
+
+        # the relative or absolute path to the penn-wsj-deps
         self.data_path = data_path
-            
+
+        # the section set is the sections that will be used as test data
+        # it can be defined as either tuple or list
+        # but cannot be both at the same time
         self.section_set = section_set
         self.section_set.sort()
         
         # track current unused test data
+        
+        # the data set which contains the current unused data
+        # it is a list of each data entry
         self.current_data_set = []
-        self.current_section = None      
+
+        # the current_section is the section
+        # in which the newest data in the current data set locates
+        self.current_section = None
+
+        # the left file list is the path of the files in the current section
+        # that has not been added to the current data set
         self.left_file_list  = [] 
         
         return            
     
     def add_file_list(self):
+        """
+        add all the file path in the next section to the left_file_list
+
+        :return: the result if the addition succeed
+        :rtype: True, if there is file path added, otherwise, False
+        """
         next_section = self.get_next_section()
         self.current_section = next_section
         if next_section == None or self.data_path == None:
@@ -37,12 +53,26 @@ class DataSet():
         return True
                       
     def get_next_data(self):
+        """
+        Return the next unused data entry
+
+        :return: An entry in the data set
+        :rtype: tuple(list, list), the first is the word list and the second is
+        the edge list
+        """
         data_set = self.current_data_set
         if data_set == []:
             data_set = self.add_data()
         return data_set.pop(0)
         
     def add_data(self):
+        """
+        add data from the next file in the left_file_list
+        that has not been added to the data set
+
+        return: if no data has been added:  None 
+                otherwise:  the udated dataset
+        """
         data_set = self.current_data_set
         
         if self.left_file_list == []:
@@ -75,6 +105,13 @@ class DataSet():
         return data_set
 
     def get_next_section(self):
+        """
+        find the next section in the section_set,
+        according to the current section
+
+        :return: the next section
+        :rtype: int
+        """
         if self.section_set == []:
             return None
         
