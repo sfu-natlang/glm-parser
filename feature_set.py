c818f611d35389fde8ee1ffff01f8df716c92cfb
zwa47 <zwa47@asb9838s-c01.csil.sfu.ca>
2014-02-25 18:06:12 -0800

    DepTree added

--- feature_set.py
+++ feature_set.py
@@ -71,7 +71,7 @@ class DependencyTree():
         :return: working array index
         :rtype: 
         """
-        return self.working_array
+        return self.working_array_index
 
     def __getitem__(self,position):
         """
@@ -91,6 +91,11 @@ class DependencyTree():
         such feature is desired please call set_pos or other methods instead.
         """
         self.working_array[self.working_array_index][position] = value
+        return
+
+    def set_word_list(self,word_list):
+        self.word_list = copy.copy(word_list)
+        self.working_array[self.WORD_ARRAY] = self.word_list
         return
 
     def set_pos_list(self,pos_list):
@@ -147,7 +152,13 @@ class DependencyTree():
         :return: A list of words
         :rtype: list(str)
         """
-        return copy.copy(word_list)
+        return copy.copy(self.word_list)
+
+    def get_word_list_ref(self):
+        return self.word_list
+
+    def get_pos_list_ref(self):
+        return self.pos_list
 
     def get_pos_list(self):
         """
@@ -157,7 +168,7 @@ class DependencyTree():
         :return: A list of POS tags
         :rtype: list(str)
         """
-        return copy.copy(pos_list)
+        return copy.copy(self.pos_list)
 
     def check_valid(self):
         t = self.is_valid()
@@ -186,15 +197,14 @@ class DependencyTree():
             (3) The first element of the POS list is a special tag "ROOT"
             (4) There is no empty string (unintialized) pos in pos_list
             (5) Edge list is a dictionary (no matter empty or not)
-            (6) Every node except the ROOT node should have at least
-                and at most one head node
+
 
         :return: A tuple. The first element is a boolean to indicate whether the
             above criterion has been satisfied. The second element is the number
             of the rule that is not satisfied, or 0 if all are satisfied
         :rtype: tuple(bool,integer)
         """
-        if len(self.ord_list) != len(self.pos_list):
+        if len(self.word_list) != len(self.pos_list):
             return (False,1)
         if self.word_list[0] != "__ROOT__":
             return (False,2)
@@ -204,6 +214,7 @@ class DependencyTree():
             return (False,4)
         if not isinstance(self.edge_list,dict):
             return (False,5)
+        """
         # Iterate through 
         for dep_index in range(1,len(self.word_list)):
             count = 0
@@ -213,6 +224,7 @@ class DependencyTree():
                     count += 1
             if count != 1:
                 return (False,6)
+        """
 
         return (True,0)
 
@@ -322,17 +334,8 @@ class DependencyTree():
         for i in self.pos_list:
             if i not in pos_set:
                 pos_set.append(i)
+                
         return pos_set
-    
-if __name__ == "__main__":
-    dt = DependencyTree("I love computer science")
-    dt.set_pos(3,'123')
-    dt.set_pos(1,'qwe')
-    dt.set_edge(0,2,'type1')
-    dt.set_edge(0,3,'type2')
-    dt.set_edge(2,4,'type97')
-    print dt.get_head_index(4)
-    print dt.get_edge_list()
         
 
 class FeatureSet():
@@ -343,21 +346,106 @@ class FeatureSet():
     def __init__(self):
         pass
 
-    def get_local_scalar(edge_tuple,word_list):
+    def get_unigram_feature_key_list(self,dep_tree):
+        """
+        Return a list of features keys, which are basuc uni-gram features.
+            xi-word, xi-pos
+            xi-word
+            xi-pos
+            xj-word, xj-pos
+            xj-word
+            xj-pos
+        Basic features are represented using a five-tuple:
+            (0,xi-word,xi-pos,xj-word,xj-pos)
+            
+        The starting 0 is used as a type identifier for basic features
+        (including both uni-gram and bi-gram).
+
+        If any entry is not used in the feature tuple it is set to empty string
+
+        :param dep_tree: A DependencyTree instance
+        :type dep_tree: DependencyTree
+        """
+        word_list = dep_tree.get_word_list_ref()
+        pos_list = dep_tree.get_pos_list_ref()
+        # Check whether the dependency tree satisfies the most fundamental
+        # requirements of a dependency tree. Especially we are concerning
+        # whether the length of pos_list and word_list are the same
+        dep_tree.check_valid()
+        
+        feature_list = []
+        # All possible combinations to the possible tags listed above
+        for i in range(len(word_list)):
+            t = (0,word_list[i],pos_list[i],'','')
+            if not t in feature_list:
+                feature_list.append(t)
+
+            t = (0,word_list[i],'','','')
+            if not t in feature_list:
+                feature_list.append(t)
+
+            t = (0,'',pos_list[i],'','')
+            if not t in feature_list:
+                feature_list.append(t)
+
+            t = (0,'','',word_list[i],pos_list[i])
+            if not t in feature_list:
+                feature_list.append(t)
+
+            t = (0,'','',word_list[i],'')
+            if not t in feature_list:
+                feature_list.append(t)
+
+            t = (0,'','','',pos_list[i])
+            if not t in feature_list:
+                feature_list.append(t)
+                
+        return feature_list
+
+    def get_bigram_feature_key_list(self,dep_tree):
+        word_list = dep_tree.get_word_list_ref()
+        pos_list = dep_tree.get_pos_list_ref()
+
+        dep_tree.check_valid()
+        feature_list = []
+
+        for i in range(0,len(word_list)):
+            for j in range(0,len(pos_list)):
+                if i == j:
+                    continue
+                if i != 0:
+                    pass
+        
+    def get_feature_key_list(self,dep_tree):
+        pass
+
+    def get_local_scalar(self,dep_tree):
         """
         Return the score of an edge given its edge elements and the context
         """
         pass
 
-    def get_local_vector(edge_tuple,word_list):
+    def get_local_vector(self,dep_tree):
         """
         Return the local feature vector of a given edge and the context
         """
         pass
 
-    def update_weight_vector(delta_list):
+    def update_weight_vector(self,dep_tree):
         """
         Update the weight vector, given a list of deltas that will be added
         up to the current weight vector
         """
         pass
+
+if __name__ == "__main__":
+    dt = DependencyTree("I love computer science")
+    dt.set_pos(4,'123')
+    dt.set_pos(3,'456')
+    dt.set_pos(2,'789')
+    dt.set_pos(1,'qwe')
+    dt.set_edge(0,2,'type1')
+    dt.set_edge(0,3,'type2')
+    dt.set_edge(2,4,'type97')
+    fs = FeatureSet()
+    print fs.get_unigram_feature_key_list(dt)
