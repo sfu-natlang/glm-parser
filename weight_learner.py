abc062c3707167407faca386ccbef65a67730226
yulanh <yulanh@sfu.ca>
2014-02-26 00:32:10 -0800

    using dependency_tree

--- weight_learner.py
+++ weight_learner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import feature_set
+import feature_set, data_set
 import eisner
 
 class WeightLearner():
@@ -7,25 +7,43 @@ class WeightLearner():
     Learns the weight of the features using maximum perceptron algorithm
     """
     def __init__(self):
-        self.feature_set = feature_set.FeatureSet()
-        return
+        pass
     
-    def learn_weight(training_data_tuple):
+    def learn_weight_source(self, section_set=None, source=None):
         """
-        Given the list of the training data, the function learns the weight 
+        Given the path and specified sections, 
+        for each dep_tree in the source
+        learn the weight for each feature in that feature set
+        
+        :param section_set: see section_set in DataSet
+        :type section_set: list(int/tuple)
+        
+        :param source: see data_path in DataSet
+        :type source: str
+        """
+        dataset = data_set.DataSet(section_set, source)
+        while dataset.has_next_data():
+            self.learn_weight(dataset.get_next_data())    
+        
+    def learn_weight(self,dep_tree):
+        """
+        Given one dependency tree, the function learns the weight 
         for each feature in the feature set
         
-        :param training_data_tuple: a tuple contains a list of the words in a 
-        sentence and a list of all the edges in the dependency structure
-        :type training_data_tuple: tuple(list, set(tuple(integer, integer)))
+        :param dep_tree: a class contains the information about the
+        word_list, pos_list, edge and the information of edge type
+        :type dep_tree: DependencyTree
         
         :return: updated feature set
         :rtype: FeatureSet
         """
-        word_list, gold_edge_set = training_data_tuple
+        fset = feature_set.FeatureSet()
+        word_list = dep_tree.get_word_list()
+        gold_edge_set = \
+            set([(head_index,dep_index) for head_index,dep_index,_ in dep_tree.get_edge_list()])
         while True:
             _, current_edge_set = \
-               eisner.EisnerParser(word_list).parse(self.feature_set.get_local_scalar)
+               eisner.EisnerParser(word_list).parse(fset.get_local_scalar)
             
             # guarantee to converge ???????
             # will the eisner calculate the different trees?
@@ -35,15 +53,15 @@ class WeightLearner():
             # calculate the global score
             # assume the length of each local vector in the same sentanse is the same
             # the truth_global_vector will change because of the change in weights
-            current_global_vector = self.get_global_vector(current_edge_set, word_list)
-            glod_global_vector = self.get_global_vector(gold_edge_set, word_list)
+            current_global_vector = self.get_global_vector(current_edge_set, dep_tree, fset)
+            glod_global_vector = self.get_global_vector(gold_edge_set, dep_tree, fset)
             delta_vector = [(truth - current)
                              for truth, current
                              in zip(glod_global_vector, current_global_vector)]
-            self.feature_set.update_weight_vector(delta_vector)
-        return self.feature_set
+            fset.update_weight_vector(delta_vector)
+        return fset
 
-    def get_global_vector(edge_set, word_list):
+    def get_global_vector(self, edge_set, dep_tree, fset):
         """
         Calculate the global vector with the current weight, the order of the feature
         score is the same order as the feature set
@@ -58,7 +76,7 @@ class WeightLearner():
         """
         global_vector = []
         for edge_tuple in edge_set:
-            local_vector = self.feature_set.get_local_vector(word_list, edge_tuple)
+            local_vector = fset.get_local_vector(dep_tree, edge_tuple)
             if global_vector == []:
                 global_vector = [0 for i in range(len(local_vector))]
             global_vector = [(g+l) for g,l in zip(global_vector, local_vector)]
