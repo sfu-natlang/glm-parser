b94c1b433d7fa2b9e1cb06dd558ba2df7da6ca02
wangziqi2013 <675973885@qq.com>
2014-03-03 02:19:04 -0800

    Add class FeatureVector

---
+++ feature_vector.py
@@ -0,0 +1,47 @@
+
+class FeatureVector():
+    """
+    Emulates a feature vector using dictionary indtead of list
+    """
+    def __init__(self):
+        self.feature_dict = {}
+        return
+
+    def __getitem__(self,feature_str):
+        return self.feature_dict[feature_str]
+
+    def __setitem__(self,feature_str,feature_count):
+        self.feature_dict[feature_str] = feature_count
+
+    def has_key(self,feature_str):
+        return self.feature_dict.has_key(feature_str)
+
+    def aggregate(self,another_fv):
+        """
+        Aggregate another feature vector into this one, no return value
+
+        :param another_fv: The feature vector that you want to aggregate
+        :type another_fv: FeatureVector
+        """
+        for i in another_fv,keys():
+            # If the feature also exists in this vector then add them up
+            if self.has_key(i):
+                self[i] += another_fv[i]
+            # If it does not exist just copy the value
+            else:
+                self[i] = another_fv[i]
+        return
+
+    def __add__(self,another_fv):
+        """
+        Arrgegate two feature vectors into one, and return a new feature vector
+        
+        :param another_fv: The feature vector that you want to aggregate
+        :type another_fv: FeatureVector
+        """
+        ret_vector = FeatureVector()
+        # These two aggregations make it slow. So do not use this overload
+        # unless necessary
+        ret_vector.aggregate(self)
+        ret_vector.aggregate(another_fv)
+        return
