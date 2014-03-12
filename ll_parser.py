780c92abd158d5356d9feeef0e85a1597caf7a6e
zwa47 <zwa47@asb9838s-c02.csil.sfu.ca>
2014-03-11 18:27:59 -0700

    wzq

--- ll_parser.py
+++ ll_parser.py
@@ -1,7 +1,53 @@
 
 from regular_expression import *
 
-def LLParser():
+class Nonterminal():
+    """
+    Nonterminal node used by LL parser
+    """
+    def __init__(self):
+        """
+        Initialize a child list. The children will be trated in order, and
+        we do not pick the longest one
+        """
+        self.child_list = []
+        pass
+
+    def append(self,child_node):
+        """
+        Add a new alternative into its child_list. All nodes in child_list is
+        treated as "or" relation rather than concatenation
+        """
+        self.child_list.append(child_node)
+        return
+
+    def parse(self,s):
+        """
+        Parse the non-terminal
+        """
+        for i in child_list:
+            ret = i.parse(s)
+            if ret == None:
+                return None
+            else:
+                return ret
+
+class Terminal():
+    """
+    Terminal node used by LL parser
+    """
+    def __init__(self,regular_exp):
+        """
+        Initialize the terminal node with a regular expression
+        """
+        self.regexp = regular_exp
+        return
+
+    def parse(self,s):
+        return self.regexp.parse(s)
+
+
+class LLParser():
     """
     A simple LL(1) parser that uses context-free grammar to describe a set
     of rules to form a string. This parser will not always return the best
@@ -31,7 +77,11 @@ def LLParser():
         :return: A list of nonterminals
         :rtype: list(str)
         """
-        return [line[0] for line in line_list] 
+        return [line[0] for line in line_list]
+
+    def process_cfg_rule(self,line,symbol_table):
+        for symbol in line:
+            
 
     def parse_cfg(self,s):
         # We process the grammar in a line basis
