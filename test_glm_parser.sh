f4d6e71a947b7d70038e59fa548312d270d3dabf
yulanh <yulanh@sfu.ca>
2014-03-19 22:11:33 -0700

    Update test_glm_parser.sh

--- test_glm_parser.sh
+++ test_glm_parser.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-#export PYTHONPATH=$PYTHONPATH:/cs/ugrad/yulanh/sfuhome/Cython-0.20.1
+#!/usr/bin/env python2.7
+export PYTHONPATH=$PYTHONPATH:/cs/natlang-projects/glm-parser/Cython-0.20.1
 
-train_sec_begin="3"
-train_sec_end="4"
-output_file_name="sec_03_04"
+train_sec_begin="2"
+train_sec_end="6"
 acc_test_sec_list="2"
 
 
-#python setup.py build_ext --inplace
+if [ "" != "$1" ]
+then
+   train_sec_begin=$1 
+fi
+
+if [ "" != "$2" ]
+then
+   train_sec_end=$2
+fi
+
+output_file_name="sec_"$train_sec_begin"_"$train_sec_end
+
+cd /home/yulanh/glm-parser
+python setup.py build_ext --inplace
 
 #train: python test_glm_parser.py -b 2 -e 2 -o sec_02 
 #accuracy test: python test_glm_parser.py -d weight_iter_1.db -o sec_02 -a 2
