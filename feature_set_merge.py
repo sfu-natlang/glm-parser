67f9a25afc0ae24dca3c9c44b88f224cd8d0eb4a
yulanh <yulanh@sfu.ca>
2014-03-21 02:21:33 -0700

    print debug info

--- feature_set_merge.py
+++ feature_set_merge.py
@@ -18,7 +18,13 @@ if __name__ == "__main__":
     fset_2 = feature_set.FeatureSet(dt,operating_mode='memory_dict')
 
     fset_1.load(db_1)
+    print "fs1 load successfully"
+
     fset_2.load(db_2)
+    print "fs2 load successfully"
 
     fset_1.merge(fset_2)
+    print "merge done"
+
     fset_1.dump(output_file)
+    print "dump done"
