2da71ed2736398f0b2053137ea4e948bba9f6ef8
yulanh <yulanh@sfu.ca>
2014-03-20 19:24:02 -0700

    accept variables

--- glm_parser_train.sh
+++ glm_parser_train.sh
@@ -5,6 +5,7 @@ export PYTHONPATH=$PYTHONPATH:/cs/natlang-projects/glm-parser/Cython-0.20.1
 train_sec_begin="2"
 train_sec_end="6"
 acc_test_sec_list="2"
+iter="0"
 test_data_path="/cs/natlang-projects/glm-parser/penn-wsj-deps/"
 output_path="/cs/natlang-projects/glm-parser/results/"
 
@@ -13,28 +14,56 @@ output_path="/cs/natlang-projects/glm-parser/results/"
 # 2.  end train section
 # 3.  iteration number (i -- indicate ith iteration)
 
-if [ "" != "$1" ]
-then
-   train_sec_begin=$1 
-fi
+HELP_MSG=" 
+This is the script for training glm parser:
 
-if [ "" != "$2" ]
-then
-   train_sec_end=$2
-fi
+options:
+	-tb:	set the begin section of training data
+	-te:	set the end section of training data
+	-op:	set the output path
+	-i:		set the iteration number (for the name of the output db)
+	-tp:	set the test data path
+	--help:	help message for the script
+"
 
-output_file_name=$output_path"sec_"$train_sec_begin"_"$train_sec_end
+opt=""
+for value in $@;
+do
+	if [ "$value" == "--help" ]
+	then
+	   echo "$HELP_MSG"
+	   exit 0
+	fi
+	
+	if [ "$opt" == "-tb" ]
+	then
+	   train_sec_begin=$value 
+	elif [ "$opt" == "-te" ]
+	then
+	   train_sec_end=$value
+	elif [ "$opt" == "-op" ]
+	then
+	   output_path=$value
+	elif [ "$opt" == "-i" ]
+	then
+	   iter=$value
+	elif [ "$opt" == "-tp" ]
+	then
+	   test_data_path=$value
+	fi
+	
+	opt=$value
 
-if [ "" != "$3" ]
-then
-   output_file_name=$output_file_name"_iter_"$3
-fi
-echo $output_file_name
-cd /home/yulanh/glm-parser
-python setup.py build_ext --inplace
+done
+
+output_file_name=$output_path"sec_"$train_sec_begin"_"$train_sec_end"_iter_"$iter
+
+
+#cd /home/yulanh/glm-parser
+#python setup.py build_ext --inplace
 
 #train: python glm_parser_train.py -b 2 -e 2 -o sec_02 
 #accuracy test: python glm_parser_train.py -d weight_iter_1.db -o sec_02 -a 2
 
-python glm_parser_train.py -b $train_sec_begin -e $train_sec_end -o $output_file_name -t $test_data_path
+python glm_parser.py -b $train_sec_begin -e $train_sec_end -o $output_file_name -t $test_data_path
 #-a $acc_test_sec_list
