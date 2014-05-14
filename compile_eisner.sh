6dcc2251fd49ab784f04d1fb77dd0124e037b573
yulanh <yulanh@sfu.ca>
2014-05-13 22:13:35 -0700

    reorganize files

--- compile_eisner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#!/usr/bin/env python2.7
-export PYTHONPATH=$PYTHONPATH:/cs/natlang-projects/glm-parser/Cython-0.20.1
-
-python setup.py build_ext --inplace
--- run_windows.bat
+++
@@ -1,3 +0,0 @@
-SET VS90COMNTOOLS=%VS100COMNTOOLS%
-python setup.py build_ext --inplace
-python glm_parser.py -b 2 -e 10 -o sec_14_14
