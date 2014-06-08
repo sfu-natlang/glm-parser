1951748fae93cd1c84423b4b5c1123c953f65e14
yulanh <yulanh@sfu.ca>
2014-06-08 10:25:04 -0700

    small change for clusters

--- scripts/qsub_train.sh
+++ scripts/qsub_train.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-code_path="/home/yulanh/glm-parser/trunk/src"
+script_path="/home/yulanh/glm-parser/trunk/scripts"
 
 cat <<EOS | qsub -
 
 #PBS -W group_list=cs-natlang
-#PBS -l pmem=16gb
-#PBS -l walltime=240:00:00
+#PBS -l pmem=200gb
+#PBS -l walltime=120:00:00
 #PBS -N glm-train
 #PBS -S /bin/csh
 
-cd $code_path
+cd $script_path
 
 ./train.sh
 
