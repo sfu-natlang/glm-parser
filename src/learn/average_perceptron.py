79b52d8dae3095018d70a86e768a79a0cc91344c
yulanh <yulanh@sfu.ca>
2014-06-08 20:31:40 -0700

    average perceptron debugged

--- src/learn/average_perceptron.py
+++ src/learn/average_perceptron.py
@@ -30,27 +30,28 @@ class AveragePerceptronLearner():
         self.weight_sum_dict.clear()
         self.last_change_dict.clear()
         self.c = 1
-        
+
         # for t = 1 ... T
         for t in range(max_iter): 
-            logging.debug("Iteration: %d ..." % t)
+            logging.debug("Iteration: %d" % t)
+            logging.debug("Data size: %d" % len(data_pool.data_list))
 
             # for i = 1 ... m
             while data_pool.has_next_data():
-
+            #for k in range(2):
+                
                 # Calculate yi' = argmax
                 data_instance = data_pool.get_next_data()
                 gold_global_vector = data_instance.gold_global_vector
                 current_global_vector = f_argmax(data_instance)
-
+                delta_global_vector = gold_global_vector - current_global_vector
+                
                 # if t != T or i != m
                 if not t == max_iter-1 or data_pool.has_next_data():
 
                     # i yi' != yi
                     if not current_global_vector == gold_global_vector:
-                        
-                        # for each dimension s in delta_global_vector
-                        delta_global_vector = gold_global_vector - current_global_vector
+                        # for each dimension s in delta_global_vector 
                         for s in delta_global_vector.keys():
                             self.weight_sum_dict[s] += self.w_vector[s] * (self.c - self.last_change_dict[s])
                             self.last_change_dict[s] += 1
@@ -68,7 +69,7 @@ class AveragePerceptronLearner():
                         self.weight_sum_dict.iadd(delta_global_vector.feature_dict)
 
             data_pool.reset()
-
+        
         self.w_vector.data_dict.clear()
         self.w_vector.data_dict.iaddc(self.weight_sum_dict, 1/self.c)
         
