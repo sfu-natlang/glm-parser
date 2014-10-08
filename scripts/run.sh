2f958a93bb445b87126a35378accbbd4a7bbacf3
anoop <anoop@rcg-queen.rcg.sfu.ca>
2014-10-08 10:17:15 -0700

    error in cmd line params; added some params

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-qsub -V -l walltime=08:00:00,nodes=1pn=1,pmem=8gb -W group_list=colony-users qsub_train.sh
+qsub -V -e $PWD -o $PWD -l walltime=24:00:00,nodes=1:ppn=1,pmem=8gb -W group_list=colony-users qsub_train.sh
 
