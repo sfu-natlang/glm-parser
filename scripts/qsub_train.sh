9f5062bf59fc244312b77848b7228273a8efbfc4
silveryfu <silveryfu@gmail.com>
2014-10-08 16:56:12 -0700

    fixed cmd line error.

--- scripts/qsub_train.sh
+++ scripts/qsub_train.sh
@@ -5,7 +5,7 @@
 ##qsub -V -l walltime=08:00:00,nodes=1pn=1,pmem=8gb -W
 ##group_list=colony-users <this-file>.sh
 
-project_path=$PWD'/src'
+project_path=$PWD'/glm-parser'
 
 source $MODULESHOME/init/bash
 
@@ -13,8 +13,8 @@ module load LANG/PYTHON/2.7.6-SYSTEM
 
 export PYTHONPATH=$PYTHONPATH:/cs/natlang-projects/glm-parser/Cython-0.20.1
 
-cd $project_path
+cd $project_path'/src'
 
 python glm_parser.py -i 2 -b 2 -e 21 -t 0,1,22,24 -p
 /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d
-$PWD'/scripts/Weight'
+$project_path'/scripts/Weight'
