26cadcd2190b0ea3898af95a7d30b4e6327c6245
silveryfu <silveryfu@gmail.com>
2014-10-23 20:02:46 -0700

    add script to generate conll tree

---
+++ src/data/generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+project_path='/cs/natlang-projects/glm-parser'
+result_path=$project_path/new_results
+
+# lossless
+python conll_tree_generator.py -t $project_path/wsj_trees/ -d $result_path/wsj_conll_tree/lossless/ -c $project_path/penn-wsj-deps/
+
+# lossy
+python conll_tree_generator.py -t $project_path/wsj_trees/ -d $result_path/wsj_conll_tree/lossy/ -c $project_path/penn-wsj-deps/ -l
+
+
+
