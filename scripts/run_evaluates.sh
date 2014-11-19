96dc56c41c8714d204452687ab718c423608e439
sunyans <sunyans@sfu.ca>
2014-11-19 15:02:06 -0800

    fix bugs in shell

--- scripts/run_evaluates.sh
+++ scripts/run_evaluates.sh
@@ -13,12 +13,13 @@ cd hvector
 python setup.py install --install-lib .
 
 cd $project_path'/src'
-python glm_parser.py -i 1 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d '/cs/natlang-projects/glm-parser/new_results/weight/OneIter'
+python glm_parser.py -i 1 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d "/cs/natlang-projects/glm-parser/new_results/weight/OneIter0"
 
-for i in {0..9}
+for i in {1..10}
 do
     echo "===============================================" >> ./glm_parser.log
-    echo "Iter $i" >> ./glm_parser.log
-    python glm_parser.py -i 1 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d '/cs/natlang-projects/glm-parser/new_results/weight/OneIter' -l "/cs/natlang-projects/glm-parser/new_results/weight/11-18-2014/Weight_Iter_$i.db"
+    echo "Iter $i :" >> ./glm_parser.log
+    j=$(($i-1))
+    python glm_parser.py -i 1 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d "/cs/natlang-projects/glm-parser/new_results/weight/OneIter$i" -l "/cs/natlang-projects/glm-parser/new_results/weight/11-18-2014/Weight_Iter_$j.db"
 done
  
