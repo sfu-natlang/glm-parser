8d2f4aa1246f4c301462e9e30525dc781a4d5877
sunyans <sunyans@sfu.ca>
2014-11-24 12:25:11 -0800

    run the evaluator only, 0 iter

--- scripts/run_evaluates.sh
+++ scripts/run_evaluates.sh
@@ -20,6 +20,6 @@ do
     echo "===============================================" >> ./glm_parser.log
     echo "Iter $i :" >> ./glm_parser.log
     j=$(($i-1))
-    python glm_parser.py -i 1 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d "/cs/natlang-projects/glm-parser/new_results/weight/OneIter$i" -l "/cs/natlang-projects/glm-parser/new_results/weight/11-18-2014/Weight_Iter_$j.db"
+    python glm_parser.py -i 0 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d "/cs/natlang-projects/glm-parser/new_results/weight/OneIter$i" -l "/cs/natlang-projects/glm-parser/new_results/weight/11-18-2014/Weight_Iter_$j.db"
 done
  
