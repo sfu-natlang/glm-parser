06e0f174f5367d9203751497b47175be0036509f
sunyans <sunyans@sfu.ca>
2014-12-01 14:15:05 -0800

    test on data pool

--- src/data/data_pool.py
+++ src/data/data_pool.py
@@ -144,7 +144,16 @@ if __name__ == "__main__":
     i = 0
     if dp.has_next_data():
         data_instance = dp.get_next_data()
-        print data_instance
+        print data_instance.word_list
+        print
+        print data_instance.pos_list
+        print
+        print data_instance.edge_list
+        print
+        print data_instance.f_vector_dict
+        print
+        print data_instance.gold_global_vector
+        
 
 
 
