83566af3c925cbcaff6cfdb910dd2010abffe5ed
sunyans <sunyans@sfu.ca>
2014-12-07 02:31:41 -0800

    Debug data_pool

--- src/data/data_pool.py
+++ src/data/data_pool.py
@@ -171,5 +171,7 @@ if __name__ == "__main__":
         print
         print data_instance.edge_list
         print
+        print data_instance.spine_list
+        print
         print data_instance.gold_global_vector.feature_dict
         
