47e657bacd422e234fc84ff30dee57c493748015
sunyans <sunyans@sfu.ca>
2014-12-06 23:50:11 -0800

    Change on script
    in order to run test with spinal feature

--- scripts/run_linearb.sh
+++ scripts/run_linearb.sh
@@ -13,4 +13,4 @@ cd hvector
 python setup.py install --install-lib .
 
 cd $project_path'/src'
-python glm_parser.py -i 10 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d '/cs/natlang-projects/glm-parser/new_results/weight/Weight'
+python glm_parser.py -i 1 -b 2 -e 3 -t 0 -p /cs/natlang-projects/glm-parser/wsj_conll_tree_lossy -d '/cs/natlang-projects/glm-parser/new_results/weight/Spinal_Weight'
