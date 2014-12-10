7c2d1a5e8a219d17aa5b4c71c41cf55db031030a
sunyans <sunyans@sfu.ca>
2014-12-10 15:24:37 -0800

    Test for 10 iters
    Generate ACC for each iter

--- scripts/run_evaluates.sh
+++ scripts/run_evaluates.sh
@@ -13,7 +13,7 @@ cd hvector
 python setup.py install --install-lib .
 
 cd $project_path'/src'
-python glm_parser.py -i 1 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d "/cs/natlang-projects/glm-parser/new_results/weight/OneIter0"
+python glm_parser.py -i 10 -b 2 -e 21 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/wsj_conll_tree_lossy/ -d "/cs/natlang-projects/glm-parser/new_results/weight/Spinal_Weight"
 
 for i in {1..10}
 do
@@ -22,6 +22,6 @@ do
     j=$(($i-1))
     
     # not specify learning section, skip learning
-    python glm_parser.py -i 0 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d "/cs/natlang-projects/glm-parser/new_results/weight/OneIter$i" -l "/cs/natlang-projects/glm-parser/new_results/weight/11-18-2014/Weight_Iter_$j.db"
+    python glm_parser.py -i 0 -t 0,1,22,24 -p /cs/natlang-projects/glm-parser/wsj_conll_tree_lossy/ -d "/cs/natlang-projects/glm-parser/new_results/weight/OneIter$i" -l "/cs/natlang-projects/glm-parser/new_results/weight/11-18-2014/Spinal_Weight$j.db"
 done
  
