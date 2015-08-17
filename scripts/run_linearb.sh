fb6d730bbc2a9aaab1a961919cb18ab07e44f331
Yizhou Wang <wyz0011@hotmail.com>
2015-08-17 15:16:21 -0700

    Add script to run parser on linearb

---
+++ scripts/run_linearb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+project_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )'/..'
+
+source $MODULESHOME/init/bash
+module load LANG/PYTHON/2.7.6-SYSTEM
+export PYTHONPATH=$PYTHONPATH:/cs/natlang-projects/glm-parser/Cython-0.20.1
+
+cd $project_path'/src'
+python setup.py build_ext --inplace
+
+cd hvector
+python setup.py install --install-lib .
+
+cd ..
+python glm_parser.py -i 30 -b 2 -e 21 -t 1,1,22,24 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ -d '/cs/natlang-projects/glm-parser/new_results/Weight' --learner=average_perceptron --fgen=english_1st_fgen --parser=ceisner
