dc59003686ac6bdb988bfca693482f97f192525c
viviankou <expandwings@live.cn>
2016-03-24 15:40:15 -0700

    change to anaconda python

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -3,12 +3,13 @@
 project_path=$(python get_path.py)
 
 source $MODULESHOME/init/bash
-module load LANG/PYTHON/2.7.6-SYSTEM
-
-export PYTHONPATH=$PYTHONPATH:/cs/natlang-projects/glm-parser/Cython-0.20.1
+module load natlang
+module load NL/LANG/PYTHON/Anaconda-2.3.0
 
 cd $project_path
 
+echo "python path"
+echo $PYTHONPATH
 echo "Compile Cython classes ..."
 python setup.py build_ext --inplace
 
@@ -16,5 +17,5 @@ python setup.py build_ext --inplace
 echo "Compile hvector ..."
 cd hvector
 python setup.py install --install-lib .
-
+cd ..
 
