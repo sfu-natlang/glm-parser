9a11b44fc2575890732ff7212df605c041a529ca
Vivian <expandwings@live.cn>
2016-03-29 16:24:44 -0700

    add run_spark_locally.sh

--- scripts/run_spark_local.sh
+++ scripts/run_spark_local.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# run spark locally
+
 project_path=$(python get_path.py)
 
 source $MODULESHOME/init/bash
 module load natlang
 module load NL/LANG/PYTHON/Anaconda-2.3.0
+#load spark 1.2.1, no need on the hadoop cluster
 module load NL/HADOOP/SPARK/1.2.1
 
 cd $project_path
@@ -18,3 +21,7 @@ cd hvector
 python setup.py install --install-lib .
 cd ..
 
+spark-submit glm_parser.py -i 10 -s 4 -p /cs/natlang-projects/glm-parser/penn-wsj-deps/ 
+--train='wsj_02[0-9][0-9].mrg.3.pa.gs.tab' 
+--test='wsj_00[0-9][0-9].mrg.3.pa.gs.tab' 
+--learner=average_perceptron --fgen=english_1st_fgen --parser=ceisner --config=config/penn2malt.config
