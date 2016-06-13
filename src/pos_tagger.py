acd09a01746fe143efa5f378a864b371e883d3c7
Jetic Gu <jetic@me.com>
2016-06-13 11:28:11 -0700

    added configuration for pos_tagger.py

--- src/pos_tagger.py
+++ src/pos_tagger.py
@@ -6,6 +6,9 @@ import debug.interact
 import os,sys
 import timeit
 import time
+import ConfigParser
+
+from ConfigParser import SafeConfigParser
 from collections import defaultdict
 
 class PosTagger():
@@ -60,21 +63,110 @@ class PosTagger():
             self.w_vector = feat_vec.data_dict
 
         acc = tester.get_accuracy(self.w_vector)
+HELP_MSG =\
+"""
 
+options:
+    -h:     Print this help message
+
+    -p:     Path to data files (to the parent directory for all sections)
+            default "./penn-wsj-deps/"
+
+    -i:     Number of iterations
+            default 1
+
+    --train=
+            Sections for training
+        Input a regular expression to indicate which files to read e.g.
+        "-r (0[2-9])|(1[0-9])|(2[0-1])/*.tab"
+
+    --test=
+            Sections for testing
+        Input a regular expression to indicate which files to test on e.g.
+        "-r (0[2-9])|(1[0-9])|(2[0-1])/*.tab"
+	
+	--tag_target=
+			Specifying tagging target file
+"""
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 1
 
 if __name__ == '__main__':
 
-    tag_file = sys.argv[1]
-    max_iter = int(sys.argv[2])
-    data_path = sys.argv[3]
-    train_regex = sys.argv[4]
-    test_regex = sys.argv[5]
+    import getopt, sys
 
-    start_time = time.time()
-    tagger = PosTagger(train_regex,test_regex,data_path,tag_file, max_iter)
-    tagger.perc_train()
-    end_time = time.time()
-    training_time = end_time - start_time
-    print "Total Training Time: ", training_time
+    train_regex = ''
+    test_regex = ''
+    max_iter = 1
+    test_data_path = ''  #"./penn-wsj-deps/"
+    data_format = 'format/penn2malt.format'
+    tag_file = '~/tagset.txt'
 
-    tagger.eveluate()
+    #tag_file = sys.argv[1]
+    #max_iter = int(sys.argv[2])
+    #data_path = sys.argv[3]
+    #train_regex = sys.argv[4]
+    #test_regex = sys.argv[5]
+    #data_format = sys.argv[6]
+
+    try:
+        opt_spec = "aht:f:r:c:p:"
+        long_opt_spec = ['train=','test=','format=', 'tag_target=']
+
+        # load configuration from file
+        #   configuration files are stored under src/format/
+        #   configuration files: *.format
+        if os.path.isfile(sys.argv[1]) == True:
+            print("Reading configurations from file: %s" % (sys.argv[1]))
+            cf = SafeConfigParser(os.environ)
+            cf.read(sys.argv[1])
+
+            train_regex    = cf.get("data", "train")
+            test_regex     = cf.get("data", "test")
+            test_data_path = cf.get("data", "data_path")
+            data_format    = cf.get("data", "format")
+
+            max_iter                             = cf.getint(    "option", "iteration")
+
+            opts, args = getopt.getopt(sys.argv[2:], opt_spec, long_opt_spec)
+        else:
+            opts, args = getopt.getopt(sys.argv[1:], opt_spec, long_opt_spec)
+
+        # load configuration from command line
+        for opt, value in opts:
+            if opt == "-h":
+                print("")
+                print("Part Of Speech (POS) Tagger")
+                print("Version %d.%d" % (MAJOR_VERSION, MINOR_VERSION))
+                print(HELP_MSG)
+                sys.exit(0)
+            elif opt == "-p":
+                test_data_path = value
+            elif opt == "-i":
+                max_iter = int(value)
+            elif opt == '--train':
+                train_regex = value
+            elif opt == '--test':
+                test_regex = value
+            elif opt == '--format':
+                data_format = value
+            elif opt == '--tag_target':
+                tag_file = value
+            else:
+                #print "Invalid argument, try -h"
+                sys.exit(0)
+
+        start_time = time.time()
+        tagger = PosTagger(train_regex, test_regex, test_data_path, tag_file, max_iter, data_format)
+        tagger.perc_train()
+        end_time = time.time()
+        training_time = end_time - start_time
+        print "Total Training Time: ", training_time
+
+        tagger.eveluate()
+    except getopt.GetoptError, e:
+        print("Invalid argument. \n")
+        print(HELP_MSG)
+        # Make sure we know what's the error
+        raise
