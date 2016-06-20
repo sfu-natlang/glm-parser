b1fc75c9fce7c0bbe9a3515db223393fb4d49f7b
Jetic Gu <jetic@me.com>
2016-06-20 14:16:09 -0700

    added more output for spark_train

--- src/learn/spark_train.py
+++ src/learn/spark_train.py
@@ -70,29 +70,37 @@ class ParallelPerceptronLearner():
         #sc = SparkContext(master=nodes_num)
         train_files= sc.wholeTextFiles(dir_name,minPartitions=10).cache()
         dp = train_files.map(lambda t: create_dp(t,fgen,format_list,comment_sign)).cache()
+
         if learner.__class__.__name__== "AveragePerceptronLearner":
+            print "[INFO]: Using Averaged Perceptron Learner"
             fv = {}
             total_sent = dp.map(get_sent_num).sum()
             c = total_sent*max_iter
-            for round in range(max_iter):
+            for iteration in range(max_iter):
+                print "[INFO]: Starting Iteration %d"%iteration
+                print "[INFO]: Initial Number of Keys: %d"%len(fv.keys())
                 #mapper: computer the weight vector in each shard using avg_perc_train
-                print "keys: %d"%len(fv.keys())
                 feat_vec_list = dp.flatMap(lambda t: learner.parallel_learn(t,fv,parser))
                 #reducer: combine the weight vectors from each shard
                 feat_vec_list = feat_vec_list.combineByKey(lambda value: (value[0], value[1], 1),
                                  lambda x, value: (x[0] + value[0], x[1] + value[1], x[2] + 1),
                                  lambda x, y: (x[0] + y[0], x[1] + y[1], x[2]+y[2])).collect()
+
                 fv = {}
                 for (feat, (a,b,c)) in feat_vec_list:
                     fv[feat] = (float(a)/float(c),b)
+                print "[INFO]: Iteration complete"
 
             self.w_vector.clear()
             for feat in fv.keys():
                 self.w_vector[feat] = fv[feat][1]/c
+
         if learner.__class__.__name__== "PerceptronLearner":
+            print "[INFO]: Using Perceptron Learner"
             fv = {}
-            for round in range(max_iter):
-                print "round: %d"%round
+            for iteration in range(max_iter):
+                print "[INFO]: Starting Iteration %d"%iteration
+                print "[INFO]: Initial Number of Keys: %d"%len(fv.keys())
                 #mapper: computer the weight vector in each shard using avg_perc_train
                 feat_vec_list = dp.flatMap(lambda t: learner.parallel_learn(t,fv,parser))
                 #reducer: combine the weight vectors from each shard
@@ -104,9 +112,11 @@ class ParallelPerceptronLearner():
                 fv = {}
                 for (feat,(a,b)) in feat_vec_list:
                     fv[feat] = float(a)/float(b)
+                print "[INFO]: Iteration complete"
             self.w_vector.clear()
             self.w_vector.iadd(fv)
-            #dump the weight vector
-            #d_filename: change the full path to hdfs file name
+
+        #dump the weight vector
+        #d_filename: change the full path to hdfs file name
         if d_filename is not None:
             self.w_vector.dump(d_filename + "_Iter_%d.db"%max_iter)
