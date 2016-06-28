97d3bf125c7d8b3e635ff907428db3e0c75938c4
Jetic <jetic@zju.edu.cn>
2016-06-28 08:44:08 -0700

    file_io, untested

---
+++ src/data/file_io.py
@@ -0,0 +1,94 @@
+import logging
+import sys, os
+import re
+import os.path
+
+def fileReadHDFS(sparkContext=None, filePath=None):
+    if filePath == None:
+        raise ValueError("FILEIO [ERROR]: Reading file not specified")
+    # Initialising
+    if sparkContext == None:
+        print ("FILEIO [INFO]: SparkContext not specified, will try to specify now")
+        try:
+            from pyspark import SparkContext,SparkConf
+            conf = SparkConf()
+            sparkContext = SparkContext(conf=conf)
+        except:
+            raise RuntimeError('FILEIO [ERROR]: SparkContext entity conflict, entity already exists')
+        externalSparkContext = False
+    else:
+        externalSparkContext = True
+    sc = sparkContext
+
+    aRdd = sc.textFile(filePath).cache()
+    fileContent = aRdd.collect()
+
+    # Finalising
+    if externalSparkContext == False:
+        sc.stop()
+        sc = None
+    return fileContent
+
+def fileWriteHDFS(sparkContext=None, filePath=None, contents=None):
+    '''
+    Acceptable contents:
+        list(array) of data
+    '''
+    if filePath == None:
+        raise ValueError("FILEIO [ERROR]: Saving file not specified")
+    if not isinstance(contents, list):
+        raise ValueError("FILEIO [ERROR]: Contents to be saved should be a list(an array)")
+    # Initialising
+    if sparkContext == None:
+        print ("FILEIO [INFO]: SparkContext not specified, will try to specify now")
+        try:
+            from pyspark import SparkContext,SparkConf
+            conf = SparkConf()
+            sparkContext = SparkContext(conf=conf)
+        except:
+            raise RuntimeError('FILEIO [ERROR]: SparkContext entity conflict, entity already exists')
+        externalSparkContext = False
+    else:
+        externalSparkContext = True
+    sc = sparkContext
+
+    try:
+        aRdd = sc.parallelize(contents).cache()
+        aRdd.coalesce(1,True).cache()
+        aRdd.saveAsTextFile(filePath)
+    except:
+        raise RuntimeError('FILEIO [ERROR]: Unable to save file to HDFS: ' + filePath)
+    return filePath + "/part-00000"
+
+def fileRead(sparkContext=None, filePath=None):
+    if filePath == None:
+        raise ValueError("FILEIO [ERROR]: File not specified")
+    if (filePath[:7] == "file://"):
+        try:
+            contents = []
+            f = open(filePath[7:])
+            for line in f:
+                contents.append(line)
+            return contents
+        except:
+            raise RuntimeError('FILEIO [ERROR]: Unable to read from local directory: ' + filePath)
+    return fileReadHDFS(sparkContext=sparkContext, filePath=filePath)
+
+def fileWrite(sparkContext=None, filePath=None, contents=None):
+    '''
+    Acceptable contents:
+        list(array) of data
+    '''
+    if filePath == None:
+        raise ValueError("FILEIO [ERROR]: saving path not specified")
+    if not isinstance(contents, list):
+        raise ValueError("FILEIO [ERROR]: Contents to be saved should be a list(an array)")
+    if (filePath[:7] == "file://"):
+        try:
+            f = open(filePath[7:], "w")
+            for line in contents:
+                f.write(line)
+            return filePath
+        except:
+            raise RuntimeError('FILEIO [ERROR]: Unable to save to local directory: ' + filePath)
+    return fileWriteHDFS(sparkContext=sparkContext, filePath=filePath, contents=contents)
