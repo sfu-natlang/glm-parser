b5ae569ae4305e56ce949d74c5372d97018395d4
Jetic Gu <jetic@me.com>
2016-06-29 13:05:48 -0700

    pep8 fix for POS Tagger

--- src/pos/pos_decode.py
+++ src/pos/pos_decode.py
@@ -1,20 +1,28 @@
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
-import pos_viterbi, pos_common
+import os
+import sys
+import inspect
 import time
+import pos_viterbi
+from pos_common import *
+
+gottenFile = inspect.getfile(inspect.currentframe())
+currentdir = os.path.dirname(os.path.abspath(gottenFile))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
 
 class Decoder():
     def __init__(self, test_list=None, tag_file="tagset.txt"):
         self.test_list = test_list
-        self.tagset = pos_common.read_tagset(tag_file)
+        self.tagset = read_tagset(tag_file)
 
     def sent_evaluate(self, result_list, gold_list):
         result_size = len(result_list)
         gold_size = len(gold_list)
-        if(result_size!=gold_size):
-            raise ValueError("TAGGER [ERRO]: Tag results do not align with gold results")
+        if result_size != gold_size:
+            raise ValueError("""
+            TAGGER [ERRO]: Tag results do not align with gold results
+            """)
         correct_num = 0
         for i in range(result_size):
             if result_list[i] == gold_list[i]:
@@ -25,10 +33,10 @@ class Decoder():
         argmax = pos_viterbi.Viterbi()
         correct_num = gold_set_size = 0
         for (word_list, pos_list, fv) in self.test_list:
-            output = argmax.perc_test(w_vec,word_list,self.tagset,"NN")
-            cnum, gnum = self.sent_evaluate(output,pos_list)
+            output = argmax.perc_test(w_vec, word_list, self.tagset, "NN")
+            cnum, gnum = self.sent_evaluate(output, pos_list)
             correct_num += cnum
             gold_set_size += gnum
-        acc = float(correct_num) /gold_set_size
+        acc = float(correct_num) / gold_set_size
         print "TAGGER [INFO]: Total Accraccy: ", acc
         return acc
--- src/pos/pos_perctrain.py
+++ src/pos/pos_perctrain.py
@@ -2,21 +2,36 @@
 # As of right now, we are using the average perceptron to do the training.
 
 from __future__ import division
-import time,copy,logging
-import os,sys,inspect
+import time
+import copy
+import logging
+import os
+import sys
+import inspect
 from collections import defaultdict
-import pos_features, pos_viterbi, pos_common
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
+import pos_features
+import pos_viterbi
+import pos_common
+
 from feature import english_1st_fgen
 from data import data_pool
 from weight import weight_vector
 
+gottenFile = inspect.getfile(inspect.currentframe())
+currentdir = os.path.dirname(os.path.abspath(gottenFile))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+
 class PosPerceptron():
 
-    def __init__(self, w_vector=None, max_iter=1, default_tag="NN", tag_file="tagset.txt"):
+    def __init__(self,
+                 w_vector=None,
+                 max_iter=1,
+                 default_tag="NN",
+                 tag_file="tagset.txt"):
+
         self.w_vector = w_vector
         self.max_iter = max_iter
         self.default_tag = default_tag
@@ -34,12 +49,15 @@ class PosPerceptron():
         c = 0
 
         for iteration in range(self.max_iter):
-            print "TAGGER [INFO]: Starting Iteration %d"%iteration
+            print "TAGGER [INFO]: Starting Iteration %d" % iteration
             log_miss = 0
 
             for (word_list, pos_list, gold_out_fv) in train_data:
-                print "\rTAGGER [INFO]: %d sentences trained"%c,
-                output = argmax.perc_test(w_vec,word_list,self.tagset,self.default_tag)
+                print "\rTAGGER [INFO]: %d sentences trained" % c,
+                output = argmax.perc_test(w_vec,
+                                          word_list,
+                                          self.tagset,
+                                          self.default_tag)
                 c += 1
 
                 if output != pos_list:
@@ -51,47 +69,61 @@ class PosPerceptron():
 
                     # Get a
                     pos_feat = pos_features.Pos_feat_gen(word_list)
-                    pos_feat.get_sent_feature(a,output)
+                    pos_feat.get_sent_feature(a, output)
 
                     # Get Delta
-                    for item in gold_out_fv:
-                        w_delta[item] += gold_out_fv[item]
-                    for item in a:
-                        w_delta[item] -= a[item]
+                    for i in gold_out_fv:
+                        w_delta[i] += gold_out_fv[i]
+                    for i in a:
+                        w_delta[i] -= a[i]
 
                     # Process Delta
-                    for item in w_delta:
-                        if w_delta[item] != 0:
+                    for i in w_delta:
+                        if w_delta[i] != 0:
 
-                            w_vec[item] += w_delta[item]
+                            w_vec[i] += w_delta[i]
 
-                            if (item) in last_iter:
-                                u_vec[item] += (c - last_iter[item]) * w_vec[item]
+                            if (i) in last_iter:
+                                u_vec[i] += (c - last_iter[i]) * w_vec[i]
                             else:
-                                u_vec[item] = w_vec[item]
+                                u_vec[i] = w_vec[i]
 
-                            last_iter[item] = c
+                            last_iter[i] = c
 
-            print "\rTAGGER [INFO]: Iteration completed, number of mistakes:", log_miss
+            print ("\rTAGGER [INFO]: Iteration completed, number of mistakes:",
+                   log_miss)
         print "TAGGER [DEBUG]: Finalising"
-        for item in w_vec:
-            if item in last_iter:
-                u_vec[item] += (c - last_iter[item]) * w_vec[item]
+        for i in w_vec:
+            if i in last_iter:
+                u_vec[i] += (c - last_iter[i]) * w_vec[i]
             else:
-                u_vec[item] = w_vec[item]
+                u_vec[i] = w_vec[i]
 
-            w_vec[item] = u_vec[item] / c
+            w_vec[i] = u_vec[i] / c
         print "TAGGER [DEBUG]: Training Completed"
         return w_vec
 
     def dump_vector(self, filename, iteration, fv):
-        print "TAGGER [INFO]: Dumping weight_vec to " + filename + "_Iter_%d.db"%iteration
+        print (
+             "TAGGER [INFO]: Dumping weight_vec to ",
+             filename,
+             "_Iter_%d.db" % iteration)
         w_vector = weight_vector.WeightVector()
         w_vector.iadd(fv)
-        w_vector.dump(filename + "_Iter_%d.db"%iteration)
+        w_vector.dump(filename + "_Iter_%d.db" % iteration)
 
-    def dump_vector_per_iter(self, filename, iteration, weight_vec, last_iter, avg_vec, num_updates):
-        print "TAGGER [INFO]: Dumping weight_vec to " + filename + "_Iter_%d.db"%iteration
+    def dump_vector_per_iter(self,
+                             filename,
+                             iteration,
+                             weight_vec,
+                             last_iter,
+                             avg_vec,
+                             num_updates):
+
+        print (
+            "TAGGER [INFO]: Dumping weight_vec to ",
+            filename,
+            "_Iter_%d.db" % iteration)
         fv = copy.deepcopy(weight_vec)
         av = copy.deepcopy(avg_vec)
         for feat in fv:
@@ -103,5 +135,5 @@ class PosPerceptron():
 
         w_vector = weight_vector.WeightVector()
         w_vector.iadd(fv)
-        i=i+1
-        w_vector.dump(filename + "_Iter_%d.db"%iteration)
+        i = i + 1
+        w_vector.dump(filename + "_Iter_%d.db" % iteration)
