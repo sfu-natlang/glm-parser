8352bef7a3e17a7c643a9dc7eacb1c037b529bde
Jingda Chen <holaelmundokingston@gmail.com>
2016-07-18 15:00:22 -0700

    pos_tagger only try top 10 tags of the previous word

--- src/pos_tagger.py
+++ src/pos_tagger.py
@@ -229,7 +229,7 @@ if __name__ == '__main__':
                           max_iter = config['iterations'])
         end_time = time.time()
         training_time = end_time - start_time
-        __logger.info("Total Training Time: ", training_time)
+        __logger.info("Total Training Time: %f" % training_time)
 
     if config['test']:
         testDataPool = DataPool(section_regex = config['test'],
