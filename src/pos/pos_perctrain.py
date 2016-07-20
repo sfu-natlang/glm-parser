2d6918b02ba9af8426850c44ae89d88a0caa37c7
Jetic Gu <jetic@me.com>
2016-07-20 16:16:00 -0700

    pos_perctrain now uses sentence for generating feature_vectors
    - pos_perctrain now uses `WeightVector` everywhere instead of
    `defaultdict`, which is just like our learners
    
    - pos_perctrain no longer uses pos_features, instead it uses the
    `Sentence` with integrated fgen to get feature vectors
    
    If it passes the test, which is currently running now, the next step
    would be to write a f_argmax function to generate the local feature
    vector, which is easy, then we’ll be able to use the real
    learners(`src/learn/*`) to do the training.

--- src/pos/pos_perctrain.py
+++ src/pos/pos_perctrain.py
@@ -8,15 +8,12 @@ import os
 import sys
 import inspect
 import logging
-from collections import defaultdict
 
-import pos_features
 import pos_viterbi
 import pos_common
 
-from feature import english_1st_fgen
 from data import data_pool
-from weight import weight_vector
+from weight.weight_vector import WeightVector
 
 gottenFile = inspect.getfile(inspect.currentframe())
 currentdir = os.path.dirname(os.path.abspath(gottenFile))
@@ -47,31 +44,29 @@ class PosPerceptron():
         if len(self.tagset) <= 0:
             raise valueError("LEARNER [ERRO]: Empty tagset")
         argmax = pos_viterbi.Viterbi()
-        w_vec = defaultdict(float)
-        u_vec = defaultdict(float)
+        w_vec = WeightVector()
+        u_vec = WeightVector()
         data_size = len(data_pool.data_list)
         last_iter = {}
         c = 0
 
         for iteration in range(self.max_iter):
-            logger.info("Starting Iteration %d" % iteration)
+            logger.debug("Iteration %d" % iteration)
+            sentence_count = 1
             log_miss = 0
 
             while data_pool.has_next_data():
                 sentence = data_pool.get_next_data()
 
-                word_list = sentence.get_word_list()
-                pos_list = sentence.column_list["POSTAG"]
-                pos_feat = pos_features.Pos_feat_gen(word_list)
-
-                gold_vector = defaultdict(int)
-                pos_feat.get_sent_feature(gold_vector, pos_list)
+                gold_vector = sentence.gold_global_vector
 
                 logger.info("Iteration %d, Sentence %d of %d, Length %d" % (
                     iteration,
                     c,
                     data_size,
-                    len(word_list) - 1))
+                    len(sentence.get_word_list()) - 1))
+                sentence_count += 1
+
                 c += 1
 
                 output = argmax.tag(sentence    = sentence,
@@ -79,27 +74,20 @@ class PosPerceptron():
                                     tagset      = self.tagset,
                                     default_tag = self.default_tag)
 
-                if output != pos_list:
+                if output != sentence.get_pos_list():
                     log_miss += 1
 
-                    # Initialise
-                    a = defaultdict(int)
-                    w_delta = defaultdict(int)
-
-                    # Get a
-                    pos_feat.get_sent_feature(a, output)
+                    # Get current_vector
+                    current_vector = sentence.get_local_vector(poslist=output)
 
                     # Get Delta
-                    for i in gold_vector:
-                        w_delta[i] += gold_vector[i]
-                    for i in a:
-                        w_delta[i] -= a[i]
+                    delta_vector = gold_vector - current_vector
 
                     # Process Delta
-                    for i in w_delta:
-                        if w_delta[i] != 0:
+                    for i in delta_vector.keys():
+                        if delta_vector[i] != 0:
 
-                            w_vec[i] += w_delta[i]
+                            w_vec[i] += delta_vector[i]
 
                             if (i) in last_iter:
                                 u_vec[i] += (c - last_iter[i]) * w_vec[i]
@@ -125,7 +113,7 @@ class PosPerceptron():
     def dump_vector(self, filename, iteration, fv):
         logger.info("Dumping weight_vec to ", filename,
                     "_Iter_%d.db" % iteration)
-        w_vector = weight_vector.WeightVector()
+        w_vector = WeightVector()
         w_vector.iadd(fv)
         w_vector.dump("file://" + filename + "_Iter_%d.db" % iteration)
 
@@ -148,7 +136,7 @@ class PosPerceptron():
                 av[feat] = fv[feat]
             fv[feat] = av[feat] / num_updates
 
-        w_vector = weight_vector.WeightVector()
+        w_vector = WeightVector()
         w_vector.iadd(fv)
         i = i + 1
         w_vector.dump("file://" + filename + "_Iter_%d.db" % iteration)
