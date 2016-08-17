02e412bda69411591247da4773b07da0574d99cd
Jetic Gu <jetic@me.com>
2016-08-17 21:03:10 +0800

    Amend test scripts

--- scripts/tests/default_sequential_tagger.sh
+++ scripts/tests/default_sequential_tagger.sh
@@ -8,7 +8,7 @@ cd ../src/
 rm glm_parser.log
 rm glm_parser.log.bak
 
-python glm_parser.py -i 1 --tagger-w-vector=$NATLANG_DATA/fv_Iter_5.db config/default.config
+python glm_parser.py -i 1 --tagger-w-vector=$NATLANG_DATA/penn2malt_tagger_Iter_1.db config/default.config
 
 ../scripts/proc_log.sh glm_parser.log
 
--- scripts/tests/default_standalone_tagger.sh
+++ scripts/tests/default_standalone_tagger.sh
@@ -11,7 +11,7 @@ rm glm_parser.log.bak
 spark-submit --driver-memory 4g   \
 			 --executor-memory 4g \
 			 --master 'local[*]'  \
-			 glm_parser.py -s 4 --tagger-w-vector=$NATLANG_DATA/fv_Iter_5.db config/default.config
+			 glm_parser.py -s 4 --tagger-w-vector=$NATLANG_DATA/penn2malt_tagger_Iter_1.db config/default.config
 
 ../scripts/proc_log.sh glm_parser.log
 
